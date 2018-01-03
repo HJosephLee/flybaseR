b363c0011bc5138f5da27f3b7fe843115c134b04
Hangnoh "Lee <hangnoh.lee@gmail.com>
2018-01-03 16:08:11 -0500

    Update id.converter.R
    Added a function that can search symbols, which cannot be converted from the FlyBase ID converter.  The new function accesses FlyBase gene reports for each gene and takes gene IDs or symbols.

--- R/id.converter.R
+++ R/id.converter.R
@@ -1,23 +1,25 @@
 #' Converting or Updating FlyBase IDs or Symbols
 #'
-#' The function takes FlyBase IDs (e.g. FBgn0000003) or Gene symbols as an input, and converts it into updated IDs or symbols.
+#' The function takes FlyBase IDs (e.g. FBgn0000003) or Gene symbols as an input, and converts it into updated IDs or symbols using the FlyBase ID converter (web).
 #' The function accesses FlyBase, so requires internet-connection.  FlyBase ID inputs are bundled as 1,000.  100 for symbols.
 #' FlyBase IDs for genes that are split into multiple genes will be concatenated with two colons (::).  Genes that does not have matching IDs will be shown as "unknown".
-#' Note: Certain gene symbols would appear as "unknown" even if the gene exists, and have FlyBase IDs. This is because the ID converter in FlyBase website cannot convert the gene.  For example, CG31976 cannot be converted by FlyBase, although you can find the gene from Jump2Genes. 
+#' Certain gene symbols would appear as "unknown" even if the gene exists, and have FlyBase IDs. This is because the ID converter in FlyBase website cannot convert the gene.  For example, CG31976 cannot be converted by FlyBase, although you can find the gene from the gene report.  Setting diehard.symbols = T will look for gene report pages of such unconvertible genes one by one.  The process is essentially slow because it accesses FlyBase for each gene. 
 
 #' @param x a vector. FlyBase IDs or names to be converted.
-#' @param output Output-types in either FlyBase IDs or symbols. "Name", "Symbol", "n", or "s" produces gene symbols as an output.  FlyBase IDs for any other characters. Default="fbid" (FlyBase IDs).  
-#' @param bundle.size The number of FlyBase IDs or symbols to be submitted to FlyBase at once. Default is 1,000 if there are less than 100 symbols.  100 if more than 1,000 symbols.  Reduce the number down if Timeout error occurs.
-#' @param DmelOnly if True, non-melanogaster gene IDs will be ignored.  Default = T.
-#' @param be.polite Numeric.  Intervals between FlyBase access for each bundle as seconds.  Default = 0.
+#' @param symbols Logical.  If TRUE, the output will be gene symbols, rather than FlyBase IDs.  Default = F
+#' @param bundle.size Numeric.  The number of FlyBase IDs or symbols to be submitted to FlyBase at once. Default is 1,000 if there are less than 100 symbols; 100 if more than 1,000 symbols.  Reduce the number down if Timeout error occurs.
+#' @param DmelOnly Logical.  If TRUE, non-melanogaster gene IDs will be ignored.  Default = T.
+#' @param polite.access Numeric.  Intervals between FlyBase access for each bundle as seconds.  Default = 0.
+#' @param diehard.symbols Logical.  If TRUE, ntervals between FlyBase access for each bundle as seconds.  Default = 0.
 #' @keywords flybase
 #' @export
 #' @examples
-#' id.converter(x, output="symbol")
-#' id.converter(x, output="fbid", bundle.size = 50, be.polite = 10)
+#' id.converter(x, symbols = T)
+#' id.converter(x, bundle.size = 50, be.polite = 10)
+#' id.converter(x, symbols = T, bundle.size = 50, diehard.symbols = T)
 
 
-id.converter <- function(x, output, bundle.size, DmelOnly, be.polite){
+id.converter <- function(x, symbols, bundle.size, DmelOnly, polite.access, diehard.symbols){
       
       if ( !requireNamespace("rvest", quietly = T) ) {
             stop("'rvest' package should be installed.", call. = F)
@@ -33,19 +35,16 @@ id.converter <- function(x, output, bundle.size, DmelOnly, be.polite){
             stop("No FlyBase IDs or Gene Symbols to be converted!!", call. = F)
       }
       
-      if ( missing(output) ){
-            output.type <- "fbid"
+      if ( missing(symbols) ){
+            symbols <- "F"
+      } 
+      
+      if (symbols == F){
             message("FlyBase IDs or Gene Symbols will be updated to the most recent version of FlyBase IDs..")
       } else {
-            if ( toupper(output) == "N" | toupper(output) == "NAME" | toupper(output) == "S" | toupper(output) == "SYMBOL" ) {
-                  output.type <- "symbol"
-                  message("FlyBase IDs or Gene Symbols will be updated to the most recent version of Gene Symbols..")
-            } else {
-                  output.type <- "fbid"
-                  message("FlyBase IDs or Gene Symbols will be updated to the most recent version of FlyBase IDs..")
-            }
+            message("FlyBase IDs or Gene Symbols will be updated to the most recent version of Gene Symbols..") 
       }
-      
+
       if ( missing(bundle.size) ){
             if ( length(x[ !grepl("FBgn", x)]) >= 100 ){ bundle.size <- 100 } else { bundle.size <- 1000 }
       }
@@ -55,8 +54,12 @@ id.converter <- function(x, output, bundle.size, DmelOnly, be.polite){
             message("non-melanogaster IDs and Symbols will be ignored.")
       }
       
-      if ( missing(be.polite) ){
-            be.polite <- 0
+      if ( missing(polite.access) ){
+            polite.access <- 0
+      }
+      
+      if ( missing(diehard.symbols) ){
+            diehard.symbols <- F
       }
       
       
@@ -89,7 +92,7 @@ id.converter <- function(x, output, bundle.size, DmelOnly, be.polite){
                   conversion.table <- conversion.table[ grepl("^D.+\\\\", conversion.table$symbols, perl=T) == F, ] # e.g. "Dyak\"
             }
             
-            if ( output.type == "fbid" ){
+            if ( symbols == F ){
                   temp.result <- sapply(as.character(temp.x), function(y){ paste(conversion.table[ y == conversion.table$submitted, 3], collapse = "::") }, simplify = T, USE.NAMES = F)
             } else {
                   temp.result <- sapply(as.character(temp.x), function(y){ paste(conversion.table[ y == conversion.table$submitted, 4], collapse = "::") }, simplify = T, USE.NAMES = F)
@@ -101,9 +104,65 @@ id.converter <- function(x, output, bundle.size, DmelOnly, be.polite){
             if ( i == 1 ){ result <- temp.result } else { result <- c(result, temp.result) }
             
             # being polite.
-            Sys.sleep(time=as.numeric(be.polite))
+            Sys.sleep(time=as.numeric(polite.access))
             
       } # for i
+      
+      
+      #################################
+      # Converting the diehard symbols
+      #################################
+      
+      if ( diehard.symbols == T & length(result[ result == "unknown"]) > 0 ){
+            
+            message("Checking FlyBase gene reports for genes that are not converted by the FlyBase ID converter.  This process is very slow and takes a long time!")
+            temp.df <- data.frame( symbols = x[ which(result == "unknown")], id = NA, newsymbols = NA, stringsAsFactors = F)
+            
+            session2 <- html_session("http://flybase.org")
+            form2 <- html_form(session2)[[1]]
+            
+            for (j in 1:nrow(temp.df)){
+                  temp.df[j, 2] <- suppressMessages(submit_form(session2, set_values(form2, context = temp.df[j,1])))$url
+                  
+                  if ( j %% 10 == 0 | j == nrow(temp.df)){
+                        message(paste("Processing ", prettyNum(j, big.mark=",", big.interval=3), " genes out of ", prettyNum(nrow(temp.df), big.mark=",", big.interval=3), sep=""))
+                  }
+                  
+            } # j
+            
+            temp.df$id <- gsub("^.+FBgn", "FBgn", temp.df$id, perl = T)
+            temp.df$id[ !grepl("FBgn", temp.df$id, fixed = T) ] <- "unknown"
+            
+            # dealing with the four most notorious symbols that FlyBase cannot handle easily. 
+            
+            temp.df[ temp.df$symbols == "E2f", 2 ] <- "FBgn0011766" # e2f indicates E2f1, but FlyBase cannot decide if it is E2f1 or E2f2 when only "E2f" is entered with a capital E. 
+            temp.df[ temp.df$symbols == "dm", 2 ] <- "FBgn0262656" # Myc
+            temp.df[ temp.df$symbols == "th", 2 ] <- "FBgn0260635" # Diap1 = thread 
+            temp.df[ temp.df$symbols == "W", 2 ] <- "FBgn0003997" # hid.  This gene used be called as Wrinkled. 
+            
+            if (symbols == F){ result[ which(result == "unknown") ] <- temp.df$id 
+            
+            } else {
+                  
+                  form3 <- html_form(session)[[2]]
+                  form3 <- set_values(form3, ids = paste(temp.df$id, collapse = "\n"))
+                  conversion.table2 <- html_table(suppressMessages(submit_form(session, form3)))[[1]]
+                  conversion.table2 <- conversion.table2[-1, ]
+                  colnames(conversion.table2) <- c("submitted", "current", "converted", "symbols")
+                  conversion.table2$submitted <- gsub(" - unknown ID", "", conversion.table2$submitted, fixed=T)
+                  conversion.table2$converted <- gsub("^.+ - unknown ID", "unknown", conversion.table2$converted, perl=T)
+                  conversion.table2$symbols <- gsub("^.+ - unknown ID", "unknown", conversion.table2$symbols, perl=T)
+                  
+                  if ( DmelOnly == T ){ 
+                        conversion.table2 <- conversion.table2[ grepl("^D.+\\\\", conversion.table2$symbols, perl=T) == F, ] # e.g. "Dyak\"
+                  }
+                  
+                  temp.df$newsymbols <- sapply( temp.df$id, function(y){ paste(conversion.table2[ y == conversion.table2$submitted, 4], collapse = "::") }, simplify = T, USE.NAMES = F)
+                  result[ which(result == "unknown") ] <- temp.df$newsymbols
+                  
+            }
+      }
+      
       
       
       ############################
